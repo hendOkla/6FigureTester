7d672805fe04d89bbc393122eb48df6bb3f05938
Hendokla99 <hendprog@gmail.com>
2024-06-18 02:13:22 +0300

    update5 18:6

--- components/Contact/ContactInfo.js
+++ components/Contact/ContactInfo.js
@@ -13,8 +13,7 @@ const ContactInfo = () => {
                                     <Icon.Mail />
                                 </div>
                                 <h3>Mail Here</h3>
-                                <p><a href="mailto:admin@6figure-earner.com">admin@6figure-earner.com</a></p>
-                                <p><a href="mailto:info@6figure-earner.com">info@6figure-earner.com</a></p>
+                                <p><a href="mailto:6figure-earner@6figure-earner.com">6figure-earner@6figure-earner.com</a></p>
                             </div>
                         </div>
 
@@ -24,7 +23,7 @@ const ContactInfo = () => {
                                     <Icon.MapPin />
                                 </div>
                                 <h3>Visit Here</h3>
-                                <p>27 Division St, New York, NY 10002, United States of America</p>
+                                <p>Dubai, United Arab Emirates</p>
                             </div>
                         </div>
 
@@ -34,8 +33,7 @@ const ContactInfo = () => {
                                     <Icon.Phone />
                                 </div>
                                 <h3>Call Here</h3>
-                                <p><a href="tel:+1234567890">+123 456 7890</a></p>
-                                <p><a href="tel:+2414524526">+241 452 4526</a></p>
+                                <p><a href="tel:+1234567890">+971545565988</a></p>
                             </div>
                         </div>
                     </div>
